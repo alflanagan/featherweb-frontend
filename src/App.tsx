b2e97fbbb3a5c2ad7f535e980fc9ec5dc87b807d
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2022-08-21 12:18:56 -0400

    fix test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from 'react'
 import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
 import './App.css'
 
 function App (): ReactElement {
